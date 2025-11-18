1f137ef33e845524209e37d4887ebc596299c362
Walson <mboew@yahoo.com>
2025-11-18 10:47:29 +0000

    Fix: amplify.yml caching

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import awsconfig from "./aws-exports.js";
 import App from './App.tsx';
 import './index.css';
 
-Amplify.configure(awsconfig);
+Amplify.configure(awsconfig); 
 
 
 createRoot(document.getElementById('root')!).render(

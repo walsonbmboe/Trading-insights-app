2d8707eeb2186c4531c05d98c114151c7501c862
Walson <mboew@yahoo.com>
2025-11-18 12:31:27 +0000

    Fix:Import ESM/CJS Conflict for aws-exports.js

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Amplify } from 'aws-amplify';
-import awsconfig from "./aws-exports.js";
+import * as awsconfig from "./aws-exports.js";
 import App from './App.tsx';
 import './index.css';
 
-Amplify.configure(awsconfig); 
+Amplify.configure(awsconfig.default); 
 
 
 createRoot(document.getElementById('root')!).render(
